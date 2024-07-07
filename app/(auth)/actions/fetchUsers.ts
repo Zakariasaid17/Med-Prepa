8fc9ba6f886cebac0f7321c4375a9c3941c27202
Noureddine said <itszakariasaid@gmail.com>
2024-07-07 02:09:27 +0100

    fix fetch users

--- app/(auth)/actions/fetchUsers.ts
+++ app/(auth)/actions/fetchUsers.ts
@@ -1,6 +1,6 @@
 "use server"
 import {prisma} from "@/lib/prisma"
-import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs/server"
 
 export const fetchUsers = async () => {
   try {
